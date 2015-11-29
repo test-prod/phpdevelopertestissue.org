4b839f581f0e81a24dd9dc16098fe349f3eeda2e
Vadim Dyubov <dyubov.v@gmail.com>
2015-11-29 13:12:09 +0300

    delete params-local from main.php

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,8 +1,7 @@
 <?php
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require(__DIR__ . '/params.php')
 );
 
 return [
